9c51f58bb5b37474709b54590cfaadae20550d6c
i5xia0 <i5xia0@icloud.com>
2017-10-18 16:01:08 +0800

    first commit

---
+++ UNN/UNN/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UNN
+//
+//  Created by 胤宏刘 on 2017/10/18.
+//  Copyright © 2017年 i5xiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UNN/UNN/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UNN
+//
+//  Created by 胤宏刘 on 2017/10/18.
+//  Copyright © 2017年 i5xiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
